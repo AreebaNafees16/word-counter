de72da644a76adafec2695209aa327a29ce19673
AreebaNafees16 <160394785+AreebaNafees16@users.noreply.github.com>
2024-04-22 14:59:14 +0500

    Implement the basic functionality to count the total number of words in the input text.

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Function to count words in a given string
+function countWords(input) {
+    const words = input.trim().split(/\s+/);
+    return words.length;
+}
+// Inquirer prompt to get user inputm
+const userAnswer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "text",
+        message: chalk.blueBright("Enter a sentence or paragraph:")
+    }
+]);
+const wordCount = countWords(userAnswer.text);
+console.log(chalk.yellow(`Your word count is: ${wordCount}`));
